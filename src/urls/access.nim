a780cf52e4f19845d58807bca5a939c43bdd1e13
upi-0 <_@upi.web.id>
2026-09-04 07:48:08 +0700

    Disable uppy endpoint.

--- src/urls/access.nim
+++ src/urls/access.nim
@@ -3,5 +3,4 @@ import prologue, controller/access
 let accessUrls* = @[
   pattern("/{garage_name}/resolve", resolve, httpMethod=[HttpGet]),
   pattern("/{garage_name}/check-status", checkStatus, httpMethod=[HttpGet]),
-  pattern("/{garage_name}/uppy", uppyEndpoint, httpMethod=[HttpPost, HttpOptions])
 ]
